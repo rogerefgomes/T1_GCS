9cc10301a894344b2ba2565ec5e130d52b785d40
Alexya Ungaratti <134217664+aungaratti@users.noreply.github.com>
2024-06-02 09:30:31 -0300

    Update Aquisicao.java
    Acrescentado "valor" e "quantidade" na classe "Aquisicao"

--- com/senalba/controleaquisicoes/model/Aquisicao.java
+++ com/senalba/controleaquisicoes/model/Aquisicao.java
@@ -5,11 +5,15 @@ public class Aquisicao
 {
     private int id;
     private String descricao;
+    private double valor;
+    private int quantidade;
 
-    public Aquisicao(int id, String descricao)
+    public Aquisicao(int id, String descricao, double valor, int quantidade)
     {
         this.id = id;
         this.descricao = descricao;
+        this.valor = valor;
+        this.quantidade = quantidade;
     }
 
     public int getId()
@@ -21,4 +25,7 @@ public class Aquisicao
     {
         return descricao;
     }
+
+
 }
+
